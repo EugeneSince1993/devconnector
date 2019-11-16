d216ec0405b58da83a1b1fb7d9b0038cf3c8d299
EugeneSince1993 <jk235@mail.ru>
2019-11-16 15:55:31 +0300

    Step 70. Deploy To Heroku. Part 2

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
 
+  /* The "*" means "all the routes except the 4 ones that we've defined above". */
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
